bd3ed4ccfe41437edc780c03a523537335c11304
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-05-27 00:11:59 +0200

    Margin prosjekt

--- src/components/Prosjekter/ProsjektCard.tsx
+++ src/components/Prosjekter/ProsjektCard.tsx
@@ -26,10 +26,10 @@ const ProsjektCard: React.FC<ProjectProps> = ({
         alt={name}
         className="w-full h-48 object-cover p-4"
       />
-      <div className="p-4">
+      <div className="p-2">
         <h1 className="text-xl text-center font-bold py-2">{name}</h1>
         <h2 className="text-md text-gray-600">{description}</h2>
-        <p className="text-sm text-gray-500">{subdescription}</p>
+        <p className="text-sm text-gray-500 mt-2">{subdescription}</p>
         <div className="flex justify-center mt-4">
           {urlwww && urlwww.length > 0 && (
             <Button href={urlwww[0].url} renderAs="a">
