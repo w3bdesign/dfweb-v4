7c7cfb1fce0d0f054e30a32cf9712bb43ccdc862
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-05-26 20:12:51 +0200

    Sanity

---
+++ studio/schema.js
@@ -0,0 +1,12 @@
+// Documents
+import project from "./documents/project";
+import category from "./documents/category";
+import page from "./documents/page";
+import navigation from "./documents/navigation";
+
+// Objects
+import link from "./objects/link";
+import pagecontent from "./objects/pagecontent";
+import herocontent from "./objects/herocontent";
+
+export const types = [project, category, page, navigation, link, pagecontent, herocontent];
---
+++ studio/schemaTypes/index.ts
@@ -0,0 +1,14 @@
+//export const schemaTypes = []
+
+// Documents
+import project from './documents/project'
+import category from './documents/category'
+import page from './documents/page'
+import navigation from './documents/navigation'
+
+// Objects
+import link from './objects/link'
+import pagecontent from './objects/pagecontent'
+import herocontent from './objects/herocontent'
+
+export const schemaTypes = [project, category, page, navigation, link, pagecontent, herocontent]
