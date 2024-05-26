754a3346ae18081a81970b23dc3a160c1627cd87
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-05-26 22:38:50 +0200

    Increase duration

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
       <motion.div
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
-        animate={{ y: 0, x: "-50%", opacity: 1, transition: { duration: 0.5 } }}
+        animate={{ y: 0, x: "-50%", opacity: 1, transition: { duration: 0.6 } }}
       ></motion.div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
@@ -54,7 +54,7 @@ export default function Header() {
               className="h-3/4 flex items-center justify-center relative"
               key={link.hash}
               initial={{ y: -100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1, transition: { duration: 0.5 } }}
+              animate={{ y: 0, opacity: 1, transition: { duration: 0.6 } }}
             >
               <Link
                 className={clsx(
