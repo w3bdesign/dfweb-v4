17428ccc8067cae8c1fbd01aa12ec89df9fbfe82
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-05-27 06:23:06 +0200

    Colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#2563eb" /* Primary button color */,
+        secondary: "#3b82f6" /* Secondary button and link color */,
+        accent: "#93c5fd" /* Focus and accent color */,
+        footer: "#1f2937" /* Footer color */,
+        background: "#e5e7eb" /* Main background color */,
+        text: "#1f2937" /* Text color */,
+        hover: "#60a5fa" /* Hover background color */,
+      },
     },
   },
   plugins: [],
