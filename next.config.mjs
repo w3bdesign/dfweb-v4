db06d3a409dfb9964bd51af7e3893dac23f75d04
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-05-30 19:51:58 +0200

    Canvas config

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
   },
 };
 
