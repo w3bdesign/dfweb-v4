983a255118c3a6be0a43b0c99ecfa374ef61fd11
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-05-30 21:00:19 +0200

    Update PDFViewer.component.tsx

--- src/components/CV/PDFViewer.component.tsx
+++ src/components/CV/PDFViewer.component.tsx
@@ -4,10 +4,14 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
+/*
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
   import.meta.url
 ).toString();
+*/
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 type PdfViewerProps = { file: string };
 
