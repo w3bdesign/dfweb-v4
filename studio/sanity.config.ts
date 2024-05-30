25d67447b696eaba7ddfcc77799601e4130e49d9
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-05-30 20:13:47 +0200

    Sanity

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,16 +1,15 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
+//import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'dfweb-v3',
-  //projectId: 'jgcz3b93',
   projectId: '41s7iutf',
   dataset: 'production',
-
-  plugins: [structureTool(), visionTool()],
+  //plugins: [structureTool(), visionTool()],
+  plugins: [structureTool()],
 
   schema: {
     types: schemaTypes,
