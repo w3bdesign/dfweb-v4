0375a85c24c4f436bc19524357c47eaac693eff3
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-01 03:33:53 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
 import { groq } from "next-sanity";
 
-import { client } from "@/lib/sanity/client";
-
 import Header from "@/components/Layout/Header.component";
-
 import Hero from "@/components/Index/Hero.component";
-
 import Testing from "@/components/Index/Testing";
+
+import { client } from "@/lib/sanity/client";
 
 export default async function PostIndex() {
   const pageContent = groq`
  *[_type == 'page' && title match 'Hjem']{"id": _id, title, hero, content}
 `;
 
-  const posts = await client.fetch<any>(pageContent);
+  const posts = await client.fetch(pageContent);
 
   console.log(posts);
 
   return (
     <>
       <Header />
-      <div className="mt-32">{posts[0].hero && <Hero content={posts[0].hero} />}</div>
-      {posts[0].content &&<Testing pageContent={posts[0].content} />}
+      <div className="mt-32">
+        {posts[0].hero && <Hero content={posts[0].hero} />}
+      </div>
+      {posts[0].content && <Testing pageContent={posts[0].content} />}
     </>
   );
 }
