6a0fec0cc53aa804c8fc66a272f0373cb80e70cb
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-06-02 05:35:24 +0200

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: 'jgcz3b93',
-    dataset: 'production'
-  }
-})
+    projectId: "jgcz3b93",
+    dataset: "production",
+  },
+});
