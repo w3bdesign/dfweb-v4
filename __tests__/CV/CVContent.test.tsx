fbac71378020d928444e429ad2bbbbb3944a9d11
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-09 03:04:06 +0200

    Tests

---
+++ __tests__/CV/CVContent.test.tsx
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from "@testing-library/react";
+
+import CVContent from "../../src/components/CV/CVContent.component";
+
+
+describe("CVContent", () => {
+  it("CVContent laster inn og kan vises", () => {
+    render(<CVContent />);
+    const cvcontent = screen.getByRole("heading", { name: /cv/i });
+    expect(cvcontent).toBeInTheDocument();
+  });
+
+  /*
+  it("PDF laster inn og kan vises", async () => {
+    render(<CVContent />);
+    const pdf = await screen.findByText(/nøkkelkvalifikasjoner/i);
+    expect(pdf).toBeInTheDocument();
+  });
+  */
+});
