63b9a8cd025a861572cb58e7e6c6a84690db1e22
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-09 04:40:39 +0200

    Update ProsjektCard.tsx

--- src/components/Prosjekter/ProsjektCard.tsx
+++ src/components/Prosjekter/ProsjektCard.tsx
@@ -21,18 +21,6 @@ const ProsjektCard: React.FC<ProjectProps> = ({
   urlwww,
   urlgithub,
 }) => {
-  const keyStr =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-  const triplet = (e1: number, e2: number, e3: number) =>
-    keyStr.charAt(e1 >> 2) +
-    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-    keyStr.charAt(e3 & 63);
-  const rgbDataURL = (r: number, g: number, b: number) =>
-    `data:image/gif;base64,R0lGODlhAQABAPAA${
-      triplet(0, r, g) + triplet(b, 255, 255)
-    }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
-
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden">
       <div className="relative w-full h-48 md:h-60">
