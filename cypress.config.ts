f4f12b5b9d2aad36d2247910ab91e430161ec81a
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-10 05:43:21 +0200

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(_on, config) {
+      // e2e testing node events setup code
+      return config;
+    },
+    baseUrl: "http://localhost:3000",
+  },
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
   },
 });
