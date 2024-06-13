9f2c3088dee3e0467d93f51d3cc68bc7c6545a7a
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-13 08:01:18 +0200

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function Custom404() {
     <div className="absolute w-full h-full">
       <ReactMatrixAnimation />
       <div className="absolute inset-0 flex flex-col items-center justify-center h-full">
-        <h1 className="text-white text-5xl m-4">You are lost, Neo</h1>
+        <h1 className="text-white text-5xl m-6">You are lost, Neo</h1>
         <Link
           href="/"
           className="glitch text-white m-4 text-xl p-6 mt-4 bg-blue-500 rounded-full hover:bg-blue-700 transition duration-300 ease-in-out transform hover:scale-105"
