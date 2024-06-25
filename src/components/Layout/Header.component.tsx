5b01acecd0e4e0c248403d09d031d97ea1921383
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-25 04:53:29 +0200

    Fix header

--- src/components/Layout/Header.component.tsx
+++ src/components/Layout/Header.component.tsx
@@ -50,7 +50,7 @@ export default function Header() {
   return (
     <header className="z-[999] relative">
       <MotionDiv
-        className="bg-primaryButtonBg fixed top-0 left-1/2 h-[4.5rem] w-full shadow rounded-none bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="bg-slate-800 bg-opacity-80 fixed top-0 left-1/2 h-[4.5rem] w-full shadow rounded-none shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-900 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -50, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1, transition: { duration: 0.6 } }}
       ></MotionDiv>
@@ -66,7 +66,9 @@ export default function Header() {
             >
               <Link
                 prefetch={true}
-                className="flex w-full items-center justify-center px-2 py-2 hover:text-slate-400 transition dark:text-gray-500 dark:hover:text-gray-300 font-semibold text-lg"
+                className={`flex w-full items-center justify-center px-2 py-2 hover:text-white transition font-semibold text-lg ${
+                  pathname === link.href ? "text-green-400" : ""
+                }`}
                 href={link.href}
               >
                 <div
