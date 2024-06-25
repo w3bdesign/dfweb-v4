7a698cf4e17d110409daab7b0b0ea0809ba4d8c4
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-25 04:51:41 +0200

    Update Header.component.tsx

--- src/components/Layout/Header.component.tsx
+++ src/components/Layout/Header.component.tsx
@@ -56,7 +56,7 @@ export default function Header() {
       ></MotionDiv>
 
       <nav className="flex fixed top-[0.65rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 w-[370px]">
-        <ul className="flex md:w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-white sm:w-[initial] sm:flex-nowrap sm:gap-5">
+        <ul className="flex md:w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-slate-200 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <MotionLi
               className="h-3/4 flex items-center justify-center relative"
@@ -66,7 +66,7 @@ export default function Header() {
             >
               <Link
                 prefetch={true}
-                className="flex w-full items-center justify-center px-2 py-2 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300 font-semibold text-lg"
+                className="flex w-full items-center justify-center px-2 py-2 hover:text-slate-400 transition dark:text-gray-500 dark:hover:text-gray-300 font-semibold text-lg"
                 href={link.href}
               >
                 <div
