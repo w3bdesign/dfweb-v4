ec5b98ab65cb15cf4c74e95b0c41bc9d56338480
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-25 04:42:55 +0200

    .

--- src/components/Prosjekter/ProsjektCard.tsx
+++ src/components/Prosjekter/ProsjektCard.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import Image from "next/image";
 
 import Button from "@/components/UI/Button.component";
+import { urlFor } from "@/lib/sanity/helpers";
+
+import styles from "./ProsjektCard.module.css";
 
 interface ProjectProps {
   id: string;
@@ -23,15 +27,26 @@ const ProsjektCard: React.FC<ProjectProps> = ({
   return (
     <div className="bg-slate-700 shadow-md rounded-lg overflow-hidden">
       <div className="relative w-full h-48 md:h-60">
-        <img
-          src={projectimage}
-          alt={name}
-          className="p-4 object-cover w-full h-full"
-        />
+        <div
+          className={`${styles.imageContainer} ${styles.imageContainerLarge}`}
+        >
+          <Image
+            className="flex justify-center text-center"
+            width="600"
+            height="300"
+            quality={100}
+            src={urlFor(projectimage).url() as string}
+            alt={name}
+            priority
+            unoptimized
+          />
+        </div>
       </div>
-      <div className="p-2 flex flex-col justify-between min-h-[250px] xl:min-h-[300px]">
+      <div className="mt-12 p-2 flex flex-col justify-between min-h-[250px] xl:min-h-[300px]">
         <div>
-          <h1 className="xl:mt-4 text-xl text-center font-bold py-2 text-slate-200">{name}</h1>
+          <h1 className="xl:mt-4 text-xl text-center font-bold py-2 text-slate-200">
+            {name}
+          </h1>
           <h2 className="text-md">{description}</h2>
           <p className="mt-4 text-sm mt-2">{subdescription}</p>
         </div>
