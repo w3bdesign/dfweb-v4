a8efb9902db069e481ebbc82f3fee39a98518888
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-27 08:45:00 +0200

    Update Header.component.tsx

--- src/components/Layout/Header.component.tsx
+++ src/components/Layout/Header.component.tsx
@@ -52,7 +52,7 @@ const Header = () => {
   return (
     <header className="z-[999] relative">
       <MotionDiv
-        className="bg-slate-800 bg-opacity-80 fixed top-0 left-1/2 h-[4.5rem] w-full shadow rounded-none shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-900 dark:border-black/40 dark:bg-opacity-75"
+        className="bg-slate-800 bg-opacity-80 fixed top-0 left-1/2 h-[4.5rem] w-screen shadow rounded-none shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{
           y: 0,
