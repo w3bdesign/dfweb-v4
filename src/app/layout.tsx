6afbf12124150ee8b5e88d005f2975c50d28e37c
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-29 08:45:57 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       <body
         className={`flex flex-col min-h-screen bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900 ${inter.className}`}
       >
-        <div className="flex-grow">{children}</div>
+        <div className="flex">{children}</div>
         <Footer />
       </body>
     </html>
