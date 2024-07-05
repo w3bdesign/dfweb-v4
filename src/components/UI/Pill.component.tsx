0953ef962583d37e963ba2f68d0a96c24151b1b8
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-05 02:11:19 +0200

    Refactor

--- src/components/UI/Pill.component.tsx
+++ src/components/UI/Pill.component.tsx
@@ -2,24 +2,41 @@ import Link from "next/link";
 import React from "react";
 
 interface PillProps {
-  href: string;
   text: string;
   className?: string;
+  href?: string;
+  onClick?: () => void;
 }
 
-const Pill: React.FC<PillProps> = ({ href, text, className = "" }) => {
-  return (
-    <Link
-      href={href}
-      className={`glitch text-white m-4 text-xl p-6 mt-4 rounded-full transition duration-300 ease-in-out transform
+/**
+ * Renders a Pill component.
+ *
+ * @param {PillProps} props - The properties for the Pill component.
+ * @param {string} props.text - The text to display in the Pill.
+ * @param {string} [props.className=''] - Additional CSS classes to apply to the Pill.
+ * @param {string} [props.href] - The URL to navigate to when the Pill is clicked.
+ * @param {() => void} [props.onClick] - The function to call when the Pill is clicked.
+ * @return {JSX.Element} The rendered Pill component.
+ */
+const Pill: React.FC<PillProps> = ({ text, className = "", href, onClick }) => {
+  const baseClasses = `glitch text-white m-4 text-xl p-6 mt-4 rounded-full transition duration-300 ease-in-out transform
                  bg-blue-600 bg-opacity-20 border-2 border-blue-800 border-opacity-30                     
                  hover:bg-blue-400 hover:bg-opacity-30
                  hover:shadow-[0_0_15px_rgba(0,255,255,0.1),0_0_20px_rgba(0,255,255,0.1),inset_0_0_20px_rgba(0,255,255,0.1)]
-                 backdrop-blur-sm ${className}`}
-      data-text={text}
-    >
+                 backdrop-blur-sm ${className}`;
+
+  if (href) {
+    return (
+      <Link href={href} className={baseClasses} data-text={text}>
+        {text}
+      </Link>
+    );
+  }
+
+  return (
+    <button className={baseClasses} onClick={onClick} data-text={text}>
       {text}
-    </Link>
+    </button>
   );
 };
 
