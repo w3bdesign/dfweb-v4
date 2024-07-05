6fd0a3a2a17a1cb7497d45275f62fe8b55187fec
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-05 02:19:54 +0200

    Refactor

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,20 +1,8 @@
-import { groq } from "next-sanity";
-
-import { client } from "@/lib/sanity/client";
-
 import Header from "@/components/Layout/Header.component";
-
-import type { Pagecontent } from "@/types/sanity.types";
 
 import KontaktContent from "@/components/Kontakt/KontaktContent.component";
 
 export default async function PostIndex() {
-  const pageContent = groq`
-*[_type == 'page']
-`;
-
-  const posts = await client.fetch<Pagecontent>(pageContent);
-
   return (
     <>
       <Header />
