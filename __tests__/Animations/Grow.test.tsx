6ed609f19ea3d1f87a75d73b0c9f8874a3ad017d
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-10 19:53:30 +0200

    Update Grow.test.tsx

--- __tests__/Animations/Grow.test.tsx
+++ __tests__/Animations/Grow.test.tsx
@@ -26,18 +26,4 @@ describe("Grow", () => {
   test("Viser children", () => {
     expect(screen.getByText("Hello World")).toBeInTheDocument();
   });
-
-  test("Returnerer children i motion.div element", () => {
-    const { container } = render(<Grow>Hello World</Grow>);
-    expect(container).toMatchInlineSnapshot(`
-      <div>
-        <div
-          data-testid="grow"
-          style="transform: scale(0)"
-        >
-          Hello World
-        </div>
-      </div>
-    `);
-  });
 });
