3ed666e2c10c4239368b96d271dc679c8acf66bd
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-21 18:59:55 +0200

    Add <title>

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -2,6 +2,13 @@ import Header from "@/components/Layout/Header.component";
 
 import KontaktContent from "@/components/Kontakt/KontaktContent.component";
 
+import { Metadata } from "next/types";
+
+export const metadata: Metadata = {
+  title: "Kontakt - Dfweb",
+  description: "Daniel Fjeldstad | Frontend Web Utvikler | Portefølje",
+};
+
 export default async function PostIndex() {
   return (
     <>
