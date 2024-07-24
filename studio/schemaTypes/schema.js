a4f21015f9f13813e92e537ada7f0c11d95a1112
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-24 05:59:04 +0200

    Add CV to Sanity

--- studio/schemaTypes/schema.js
+++ studio/schemaTypes/schema.js
@@ -1,12 +1,13 @@
 // Documents
-import project from "./documents/project";
-import category from "./documents/category";
-import page from "./documents/page";
-import navigation from "./documents/navigation";
+import project from './documents/project'
+import category from './documents/category'
+import page from './documents/page'
+import navigation from './documents/navigation'
+import cv from './documents/cv'
 
 // Objects
-import link from "./objects/link";
-import pagecontent from "./objects/pagecontent";
-import herocontent from "./objects/herocontent";
+import link from './objects/link'
+import pagecontent from './objects/pagecontent'
+import herocontent from './objects/herocontent'
 
-export const types = [project, category, page, navigation, link, pagecontent, herocontent];
+export const types = [project, category, page, navigation, cv, link, pagecontent, herocontent]
