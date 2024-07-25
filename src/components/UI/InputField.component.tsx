e83f9e2ad33e9896607fa7ee9504cf8580b8424a
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-25 03:57:02 +0200

    Fix background

--- src/components/UI/InputField.component.tsx
+++ src/components/UI/InputField.component.tsx
@@ -21,7 +21,7 @@ const InputField = ({
   ...props
 }: IInputProps) => {
   const sharedClasses =
-    "cursor-pointer peer block text-xl w-64 p-2 bg-slate-700 text-slate-200 border-gray-500 border rounded border-opacity-50 outline-none focus:border-slate-200 placeholder-gray-300 placeholder-opacity-0 transition duration-200";
+    "cursor-pointer peer block text-xl w-64 p-2 bg-gray-800 text-slate-200 border-gray-500 border rounded border-opacity-50 outline-none focus:border-slate-200 placeholder-gray-300 placeholder-opacity-0 transition duration-200";
 
   return (
     <div className="relative my-2 flex justify-center">
@@ -50,7 +50,7 @@ const InputField = ({
         )}
         <label
           htmlFor={htmlFor}
-          className={`absolute cursor-pointer left-1 top-0 z-10 origin-[0] -translate-y-8 scale-75 transform bg-slate-700 px-2 text-slate-200 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-10 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-primary-600 ${
+          className={`absolute cursor-pointer left-1 top-0 z-10 origin-[0] -translate-y-8 scale-75 transform bg-gray-800 px-2 text-slate-200 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-10 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-primary-600 peer-focus:bg-gray-800 ${
             type === "textarea" ? "cursor-pointer peer-focus:-top-4" : ""
           }`}
         >
