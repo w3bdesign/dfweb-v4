1f01e7ef20fdb808d33b07e183a50e211c70362b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-08-31 03:35:21 +0200

    Typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-const nextJest = require("next/jest");
+import type { JestConfigWithTsJest } from 'ts-jest';
+import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 });
 
 // Add any custom config to be passed to Jest
-const customJestConfig = {
+const customJestConfig: JestConfigWithTsJest = {
   preset: "ts-jest/presets/js-with-babel-esm",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: ["<rootDir>/cypress/", "<rootDir>/playwright/"],
@@ -21,4 +21,4 @@ const customJestConfig = {
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
+export default createJestConfig(customJestConfig);
