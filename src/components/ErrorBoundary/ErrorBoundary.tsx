56451f3762846ecf78a78ce4f9fd94db77c26b17
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-09-01 03:52:45 +0200

    Update ErrorBoundary.tsx

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -2,6 +2,7 @@
 
 import React, { ReactNode } from 'react';
 import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
+import ReactMatrixAnimation from "@/components/Animations/Matrix.component";
 
 interface ErrorFallbackProps {
   error: Error;
@@ -9,17 +10,20 @@ interface ErrorFallbackProps {
 
 const ErrorFallback: React.FC<ErrorFallbackProps> = ({ error }) => {
   return (
-    <div className="text-center py-10" role="alert">
-      <h2 className="text-2xl font-bold text-red-500">Oops! Something went wrong.</h2>
-      <p className="mt-2 text-gray-600">
-        {error.message || 'An unexpected error occurred.'}
-      </p>
-      <button
-        onClick={() => window.location.reload()}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-      >
-        Refresh Page
-      </button>
+    <div className="absolute w-full h-full">
+      <ReactMatrixAnimation />
+      <div className="absolute inset-0 flex flex-col items-center justify-center h-full">
+        <h1 className="text-red-500 text-5xl m-6">Oops! Something went wrong.</h1>
+        <p className="text-white text-xl mb-6">
+          {error.message || 'An unexpected error occurred.'}
+        </p>
+        <button
+          onClick={() => window.location.reload()}
+          className="px-6 py-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors duration-300"
+        >
+          Refresh Page
+        </button>
+      </div>
     </div>
   );
 };
