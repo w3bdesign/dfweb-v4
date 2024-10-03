bd889b506a68377acff449a8a5fa49a0487611a9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-10-03 22:46:43 +0200

    Typescript Cvdata

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -5,14 +5,31 @@ import { client } from "@/lib/sanity/client";
 import { cvQuery } from "@/lib/sanity/queries";
 
 import { Metadata } from "next/types";
+import { Pagecontent } from "@/types/sanity.types";
 
 export const metadata: Metadata = {
   title: "CV - Dfweb",
   description: "Daniel Fjeldstad | Frontend Web Utvikler | Portefølje",
 };
 
+interface CVData {
+  keyQualifications: string[];
+  experience: Array<{
+    period: string;
+    company: string;
+    role: string;
+    description: Pagecontent["text"];
+  }>;
+  education: Array<{
+    period: string;
+    institution: string;
+    degree: string;
+    description: Pagecontent["text"];
+  }>;
+}
+
 export default async function CVPage() {
-  const cvData = await client.fetch(cvQuery);
+  const cvData: CVData = await client.fetch(cvQuery);
 
   return (
     <RootLayout>
