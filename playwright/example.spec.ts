a593e3d2336c9d2298221bdab4332ac6c8291b63
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-10-06 23:20:40 +0200

    .

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -1,9 +1,10 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("test", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await expect(page.getByRole("heading", { name: "Hei!" })).toBeVisible();
+test('homepage content and structure', async ({ page, browserName }) => {
+  // Note: WebKit tests are skipped on Windows due to SSL certificate issues
+  await page.goto('http://localhost:3000/');
+  await expect(page.getByRole('heading', { name: 'Hei!' })).toBeVisible();
   await expect(
-    page.getByRole("heading", { name: "Jeg heter Daniel Fjeldstad og" })
+    page.getByRole('heading', { name: 'Jeg heter Daniel Fjeldstad og' })
   ).toBeVisible();
 });
