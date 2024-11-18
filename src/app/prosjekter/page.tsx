8cd3671a9621c9a94f5b34f34cbab68c3956ca32
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-11-18 01:55:53 +0100

    Margin prosjekter

--- src/app/prosjekter/page.tsx
+++ src/app/prosjekter/page.tsx
@@ -48,7 +48,7 @@ export default async function Prosjekter() {
               </div>
             </div>
           )}
-          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-2 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-2 gap-8 mb-8">
             {nonFeaturedProjects.map((project) => (
               <ProsjektCard key={project.id} {...project} />
             ))}
