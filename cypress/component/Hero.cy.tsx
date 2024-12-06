ccd7f84a499a4b33c02b662dbd58315e53101a05
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-12-06 03:12:50 +0100

    Fix test

--- cypress/component/Hero.cy.tsx
+++ cypress/component/Hero.cy.tsx
@@ -1,15 +1,22 @@
 /// <reference types="cypress"/>
 
 import Hero from "../../src/components/Index/Hero.component";
+import type { Herocontent } from "../../src/types/sanity.types";
 
-const content = [
+const content: (Herocontent & { _key: string })[] = [
   {
+    _type: "herocontent",
+    _key: "hero1",
     text: "Hei!",
   },
   {
+    _type: "herocontent",
+    _key: "hero2",
     text: "Jeg heter Daniel Fjeldstad og er en webutvikler.",
   },
   {
+    _type: "herocontent",
+    _key: "hero3",
     text: "Jeg kan PHP, Wordpress, Javascript, Typescript, React, Vue, Docker, Photoshop og mye mer.",
   },
 ];
