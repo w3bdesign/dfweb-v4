70401a7ed7b3774996e4fc72d8d02413628492db
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-04-25 01:15:42 +0200

    💄 ui: replace gradient background with image for mobile view
    This change updates the MobileBackground component to display a static
    background image instead of the previous gradient background, enhancing
    the visual appearance on mobile devices.

--- src/components/Index/MobileBackground.component.tsx
+++ src/components/Index/MobileBackground.component.tsx
@@ -1,9 +1,16 @@
 /**
- * MobileBackground component that renders a background gradient for mobile devices.
+ * MobileBackground component that renders a background image for mobile devices
  * @returns {JSX.Element} The rendered MobileBackground component
  */
 const MobileBackground = () => (
-  <div className="absolute inset-0 -top-2 -mb-2 bg-gradient-to-b from-[#000000] to-emerald-800 md:hidden" />
+  <div
+    className="absolute inset-0 md:hidden bg-cover bg-center bg-no-repeat pb-4"
+    style={{
+      backgroundImage: "url('/images/mobilbg.webp')",
+      top: "-0.5rem",
+      marginBottom: "0.5rem",
+    }}
+  />
 );
 
 export default MobileBackground;
