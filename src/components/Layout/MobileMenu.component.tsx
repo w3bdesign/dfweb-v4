4d0a553da3c54b09efd5d0549d696af6d700630a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-04-27 03:28:23 +0200

    🔥 cleanup: remove unused import in MobileMenu component
    This commit removes the unnecessary React useEffect import from the
    MobileMenu component as it wasn't being utilized anywhere in the code.

--- src/components/Layout/MobileMenu.component.tsx
+++ src/components/Layout/MobileMenu.component.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AnimatePresence, useCycle, motion } from "motion/react";
-import { useEffect } from "react";
 
 import Hamburger from "./Hamburger.component";
 
