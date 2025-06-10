200738e869f320a1cf385c6546873fda6d605c23
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-06-10 23:37:33 +0200

    🔥 cleanup: remove unnecessary comment in cypress layout test
    The comment stating "Set mobile viewport" was redundant as the purpose
    of the cy.viewport method is self-explanatory in the context of the
    Cypress test.

--- src/e2e/cypress/e2e/layout.cy.ts
+++ src/e2e/cypress/e2e/layout.cy.ts
@@ -2,7 +2,7 @@
 
 describe("Layout", () => {
   beforeEach(() => {
-    cy.viewport("iphone-x"); // Set mobile viewport
+    cy.viewport("iphone-x");
     cy.visit("http://localhost:3000");
   });
 
