e3d7c9c8d3fc1b2df7286334187d931113623fa4
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-06-10 15:00:05 +0200

    🔧 chore: import component types in cypress support
    The component.ts file now imports component.d.ts file to ensure proper
    type definitions are available. This removes the redundant global
    declaration that was previously defined inline.

--- src/e2e/cypress/support/component.ts
+++ src/e2e/cypress/support/component.ts
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
+import "./component.d.ts";
 
 import "@/styles/globals.css";
 
@@ -25,17 +26,7 @@ import { mount } from "cypress/react";
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
 
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount;
-    }
-  }
-}
-
 Cypress.Commands.add("mount", mount);
 
 // Example use:
 // cy.mount(<MyComponent />)
-
-export {};
