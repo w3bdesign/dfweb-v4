30941b47f4be2976a17062837fe4fd42a6c205ff
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2026-05-26 02:32:05 +0200

    ♿️ access: replace div with semantic main element
    Improve semantic HTML structure in 404 page by replacing the
    root div wrapper with a main element for better accessibility
    and adherence to HTML5 standards.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,12 +3,12 @@ import Pill from "@/components/UI/Pill.component";
 
 export default function Custom404() {
   return (
-    <div className="absolute w-full h-full">
+    <main className="absolute w-full h-full">
       <ReactMatrixAnimation />
       <div className="absolute inset-0 flex flex-col items-center justify-center h-full">
         <h1 className="text-white text-5xl m-6">Har du gått deg vill, Neo?</h1>
         <Pill href="/" text="Returner til Matrix" />
       </div>
-    </div>
+    </main>
   );
 }
